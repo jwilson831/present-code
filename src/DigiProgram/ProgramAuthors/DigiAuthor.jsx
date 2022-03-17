9a646b584277d0b01b805fb6df5dbb0fe4abc776
William Mosto <will.mosto@gmail.com>
2022-03-17 18:31:37 -0400

    Edits

--- src/DigiProgram/ProgramAuthors/DigiAuthor.jsx
+++ src/DigiProgram/ProgramAuthors/DigiAuthor.jsx
@@ -22,15 +22,15 @@ const DigiAuthor = () => {
           </h3>
           <br />
           <h2>Unpacking World Affairs</h2>
-          <h3>for Global Business Leaders</h3>
+          <h4>for Global Business Leaders</h4>
           <br />
           <h3>
-            An Hour with <br /> Ambassador Cameron Munter (retired)
+            <p style={{whiteSpace: "nowrap"}}>Ambassador Cameron Munter (retired)</p>
+            
             <br />
-            and his Special Guests
+            <h4>Along with Special Guests</h4>
             <br />
-            <br />
-            View Streamed or On Demand Worldwide
+            <h5>View Streamed or On Demand Worldwide</h5>
           </h3>
 
           {/* <h3>- </h3>

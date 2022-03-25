2d442f85623016129bef8876f0e4e1695698554a
William Mosto <will.mosto@gmail.com>
2022-03-25 17:54:16 -0400

    viewall autoscroll

--- src/ViewAll/ViewAllBtn.jsx
+++ src/ViewAll/ViewAllBtn.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { scrollToTop } from '../util/scrollToTop';
 
 function ViewAllBtn(props){
     return(
         <div className="view-all-container">
             <div className="view-all comments-box">
                 {props.linkTo === "article" ?
-                    <Link to="/index/articles"><p className="com-text text-light">View All</p></Link>
+                    <Link to="/index/articles"><p onClick={() => scrollToTop()} className="com-text text-light">View All</p></Link>
                     :
-                    <Link to="/index/conferences"><p className="com-text text-light">Conference Calendar</p></Link>
+                    <Link to="/index/conferences"><p onClick={() => scrollToTop()} className="com-text text-light">Conference Calendar</p></Link>
                 }
             </div>
         </div>
